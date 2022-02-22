f7a90537d5ca4076f44f91b810f62ebdf1f529f9
William Hart <hart.wl@gmail.com>
2022-02-23 08:18:04 +1100

    Set correct pins for GPIO

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use hal::{
     rcc::HSEBypassMode,
     usb::{Peripheral, UsbBus},
 };
-use switch_hal::{IntoSwitch, OutputSwitch};
+use switch_hal::IntoSwitch;
 use usb_device::{class_prelude::UsbBusAllocator, prelude::*};
 use usbd_hid::descriptor::generator_prelude::*;
 use usbd_hid::descriptor::KeyboardReport;
@@ -51,22 +51,22 @@ fn main() -> ! {
 
     /* Set up GPIO */
     let gpioa = dp.GPIOA.split(&mut clocks);
+    let gpiob = dp.GPIOB.split(&mut clocks);
 
     /* Set up "button" pins */
     // TODO: Get correct pins
-    let (btn_left, btn_right, mut led, mut usb_dp) = cortex_m::interrupt::free(move |cs| {
+    let (btn_left, btn_right, mut usb_dp) = cortex_m::interrupt::free(move |cs| {
         (
             gpioa
-                .pa9
+                .pa5
                 .into_pull_up_input(cs)
                 .downgrade()
                 .into_active_low_switch(),
-            gpioa
-                .pa10
+            gpiob
+                .pb1
                 .into_pull_up_input(cs)
                 .downgrade()
                 .into_active_low_switch(),
-            gpioa.pa8.into_push_pull_output(cs).into_active_low_switch(),
             gpioa.pa12.into_push_pull_output(cs),
         )
     });
@@ -74,9 +74,6 @@ fn main() -> ! {
     /* create the StatefulKeys */
     let mut key_left = StatefulKey::new(btn_left, 0x14_u8);
     let mut key_right = StatefulKey::new(btn_right, 0x08_u8);
-
-    /* Set up LED pin for status */
-    led.off().ok();
 
     // BluePill board has a pull-up resistor on the D+ line.
     // Pull the D+ pin down to send a RESET condition to the USB bus.
